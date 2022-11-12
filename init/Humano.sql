837553e9a8fca9ecbd0cf60a297c1b507061bb01
Joan Luna <alexis_dx8@hotmail.com>
2022-11-11 17:35:17 -0700

    agregando llegando db en docker

---
+++ init/Humano.sql
@@ -0,0 +1,28 @@
+create table Humano (
+	id int NOT NULL AUTO_INCREMENT,
+	Nombre VARCHAR(50),
+	Sexo VARCHAR(50),
+	Edad INT,
+	Altura INT,
+	Peso INT,
+    PRIMARY KEY(id)
+);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (1, 'joan', 'male', 23, 139, 89);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (2, 'Nadean', 'Female', 23, 139, 89);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (3, 'Willis', 'Male', 28, 160, 130);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (4, 'Ameline', 'Female', 25, 191, 107);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (5, 'Antonella', 'Female', 40, 199, 149);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (6, 'Evyn', 'Male', 40, 164, 104);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (7, 'Chery', 'Female', 23, 143, 114);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (8, 'Moll', 'Female', 31, 189, 120);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (9, 'Berky', 'Male', 25, 171, 109);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (10, 'Enrique', 'Male', 16, 167, 106);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (11, 'Judd', 'Male', 34, 197, 102);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (12, 'Inez', 'Female', 31, 140, 87);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (13, 'Dalt', 'Male', 28, 169, 115);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (14, 'Peri', 'Female', 35, 186, 64);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (15, 'Darius', 'Male', 20, 179, 137);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (16, 'Gabrielle', 'Female', 20, 154, 74);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (17, 'Ebeneser', 'Male', 36, 228, 50);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (18, 'Margarete', 'Female', 26, 223, 110);
+insert into Humano (id, Nombre, Sexo, Edad, Altura, Peso) values (19, 'Kass', 'Female', 16, 152, 76);
